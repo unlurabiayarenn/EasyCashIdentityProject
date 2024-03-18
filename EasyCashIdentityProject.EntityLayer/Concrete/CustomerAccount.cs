9bd0e0db0cfc104fd6769a99fd1ef4df818d94f2
Rabia Yaren  Ünlü <unlurabiayaren@gmail.com>
2024-03-18 09:13:09 +0300

    first migration

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+	public class CustomerAccount
+	{
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; } //hesapnumarası
+        public string CustomerAccountCurrency { get; set; } //döviz türü
+        public decimal CustomerAccountBalance { get; set; } //bakiye
+        public string BankBranch { get; set; } //şube
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+
+
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+	public class CustomerAccountProcess
+	{
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; } //işlem türü
+        public decimal Amount { get; set; } //miktar, ne kadar para gönderildi
+        public DateTime ProcessDate { get; set; } //tarih
+
+
+	}
+}
