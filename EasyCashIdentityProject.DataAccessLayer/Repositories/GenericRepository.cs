d2cedeb137b2728e0eed4f5179a774ed8080c1a6
Rabia Yaren  Ünlü <unlurabiayaren@gmail.com>
2024-03-21 16:50:04 +0300

    Interface ve Repositorylerin Yazılması

---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		public void Delete(T t)
+		{
+			//throw new NotImplementedException();
+			using var context = new Context();
+			context.Set<T>().Remove(t);
+			context.SaveChanges();
+		}
+
+		public T GetByID(int id)
+		{
+			//throw new NotImplementedException();
+			using var context = new Context();
+			return context.Set<T>().Find(id);
+		}
+
+		public List<T> GetList()
+		{
+			//throw new NotImplementedException();
+			using var context = new Context();
+			return context.Set<T>().ToList();
+		}
+
+		public void Insert(T t)
+		{
+			//throw new NotImplementedException();
+			using var context = new Context();
+			context.Set<T>().Add(t);
+			context.SaveChanges();
+		}
+
+		public void Update(T t)
+		{
+			//throw new NotImplementedException();
+			using var context = new Context();
+			context.Set<T>().Update(t);
+			context.SaveChanges();
+		}
+	}
+}
